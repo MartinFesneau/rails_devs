a696ea7e6d2a215916596eb21d650e38d85acd3e
Martin Fesneau <martin.fesneau@gmail.com>
2021-01-04 15:39:38 +0100

    initial commit with tailwind css and all gems

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_04_140329) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
